8944c6a323837edd02cd1b1aae7c69a2c738b215
Kvarsha07 <you@example.com>
2024-08-16 21:36:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+
+import './App.css';
+
+function App(){
+  
+    return (
+      <div className='container'>
+        <div className='nav-item'>
+          <h2>CNAPP Dashboard</h2>
+          <button>Add Widget +</button>
+          <button > o</button>
+          <button > ;;</button>
+         <div>
+           <AccessTimeIcon/>
+         <select>
+            <option>1</option>
+            <option>1</option>
+            <option>1</option>
+          </select>
+         </div>
+          
+
+        </div>
+        
+        
+      </div>
+
+    );
+  }
+
+
+export default App;
